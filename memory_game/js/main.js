8e1fe2a897e127ccc824d49d6a3b7cc890ab3217
Bheki Maenetja <bhekimaenetja@Bhekis-MacBook-Pro.local>
2019-10-09 15:43:34 +0200

    created logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!!!!")
+
+var cardOne, cardTwo, cardThree, cardFour;
+
+cardOne = 'queen';
+cardTwo = 'king';
+cardThree = 'queen';
+cardFour = 'king';
+
+console.log(cardOne, cardThree);
