bed33902a4f03a499632824b06bb085afc4dba29
Bheki Maenetja <bhekimaenetja@Bhekis-MacBook-Pro.local>
2019-10-17 10:23:26 +0200

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,24 +2,24 @@ console.log("Up and running!!!!")
 
 var cards = ['queen','queen','king','king'];
 var cardsInPlay = [];
-var cardOne;
-cardOne = cards[0];
 
-cardsInPlay.push(cardOne)
-
-console.log('User flipped ' + cardOne);
-
-var cardTwo;
-cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!!!");
-  } else {
-    alert("Sorry, try again");
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!!!");
+    } else {
+      alert("Sorry, try again");
+    }
   }
 }
+
+function flipCard(cardId) {
+  var cardOne, cardTwo
+
+  console.log(`User flipped ${cards[cardId]}`);
+  cardsInPlay.push(cards[cardId])
+  checkForMatch()
+}
+
+flipCard(0)
+flipCard(2)
