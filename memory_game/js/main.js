301b61ae8933080dea92a482344b1cee5335694c
Bheki Maenetja <bhekimaenetja@Bhekis-MacBook-Pro.local>
2019-10-18 16:46:59 +0200

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,36 @@
 console.log("Up and running!!!!")
 
-var cards = ['queen','queen','king','king'];
+var cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: "images/queen-of-hearts.png",
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: "images/queen-of-diamonds.png",
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: "images/king-of-hearts.png",
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: "images/king-of-diamonds.png",
+  }
+];
 var cardsInPlay = [];
 
-function checkForMatch() {
+function flipCard() {
+  var cardId = this.getAttribute('data-id')
+  console.log(`User flipped ${cards[cardId].rank}`);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank)
+  this.setAttribute('src', cards[cardId].cardImage)
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match!!!");
@@ -13,13 +40,14 @@ function checkForMatch() {
   }
 }
 
-function flipCard(cardId) {
-  var cardOne, cardTwo
-
-  console.log(`User flipped ${cards[cardId]}`);
-  cardsInPlay.push(cards[cardId])
-  checkForMatch()
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+   var cardElement = document.createElement('img');
+   cardElement.setAttribute('src', 'images/back.png');
+   cardElement.setAttribute('data-id', i);
+   cardElement.addEventListener('click', flipCard);
+   document.querySelector('#game-board').appendChild(cardElement);
+  }
 }
 
-flipCard(0)
-flipCard(2)
+createBoard();
